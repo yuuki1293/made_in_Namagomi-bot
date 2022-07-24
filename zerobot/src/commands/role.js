e2fd14a22c83680339811a8a4ba17cc0a5f41481
ikazuchizero <90844161+ikazuchizero@users.noreply.github.com>
2022-07-25 03:36:25 +0900

    これをいい感じに改変していく

--- zerobot/src/commands/role.js
+++ zerobot/src/commands/role.js
@@ -1,1 +1,14 @@
-//ぼけが
+const handleReaction = async (channnelID,messageID,callback) => {
+    const cannnel = await Client.channnels.fetch(channnelID)
+    const message = await channnel.messages.fetch(messageID)
+    const collector = message.createReactionCollector(() => true)
+    collector.on("collect",(reaction,user) => callback(reaction,user))
+}
+
+client.on("ready",() => {
+    handleReaction("channnel id","message id"),(reaction,user) => {
+        if (reaction.emoji.name === ":one:") {
+            reaction.message.guild.member(uesr).roles.add("role id")
+        }
+    }
+})
